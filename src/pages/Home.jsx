2ec42773a624eb56cbfbdbb08f8696def201200f
Loyoenock <loyoenock@gmail.com>
2024-02-01 05:26:05 +0300

    Light added to island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
-import Island from '../models/island'
+import Island from '../models/Island'
+import Sky from '../models/sky'
+
 
  {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP
@@ -31,12 +33,11 @@ const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize(
         camera={{ near: 0.1, far: 1000 }}
       >
       <Suspense fallback={<Loader/>}>
-        <directionalLight />
-        <ambientLight />
-        <pointLight />
-        <spotLight />
-        <hemisphereLight />
+        <directionalLight position={[1,1,1]} intensity={2} />
+        <ambientLight intensity={0.5} />
+        <hemisphereLight skyColor="@#b1e1ff" groundColor="#000000" intensity={1} />
 
+        <Sky />
         <Island 
           scale={islandScale}
           position={islandPosition}
